66171ee9dd1c3880408d24df6967d0f2aad94a3c
ccorreas <correasberenguer@gmail.com>
2026-02-10 18:38:11 +0100

    feat/negative-number-not-allowed

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,8 @@
 public class Calculator {
     public int add(int a, int b) {
-        System.out.println("Adding numbers");
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException();
+        }
         return a + b;
     }
 }
